0ecb4cac66dc40f54c05ddc20cc5d8a5e4e0cea5
wathone <mg.wathonekhant@gmail.com>
2022-11-19 18:44:47 +0630

    updating design and route

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -1,14 +1,5 @@
 <x-layout>
-    <x-slot name="title"><title>Blogs</title></x-slot>
-        @foreach($blogs as $blog)
-        <div>
-            <h1><a href="blogs/{{$blog->slug}}">{{$blog->title}}</a></h1>
-            <p><a href="/categories/{{$blog->category->slug}}">{{$blog->category->name}}</a></p>
-            <div>
-                <h4>Author -<a href="/users/{{$blog->author->username}}">{{$blog->author->name}}</a></h4>
-                <p>published at ->{{$blog->created_at->diffForHumans()}}</p>
-                <p>{{$blog->intro}}</p>
-            </div>
-        </div>
-        @endforeach
-</x-layout>
+    <x-hero></x-hero>
+   <x-blogs-section :blogs="$blogs" :categories="$categories" :currentCategory="$currentCategory??null"></x-blogs-section>
+    <x-subscribe></x-subscribe>
+</x-layout>    
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Models\Blog;
 use App\Models\Category;
 use App\Models\User;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 /*
 |--------------------------------------------------------------------------
@@ -19,22 +18,27 @@ use Illuminate\Support\Facades\Log;
 
 Route::get('/', function () {
     return view('blogs',[
-        'blogs'=>Blog::latest()->get()
+        'blogs'=>Blog::latest()->get(),
+        'categories'=>Category::all()
     ]);
 });
 Route::get('/blogs/{blog:slug}', function(Blog $blog){
 
     return view('blog',[
-        'blog'=>$blog
+        'blog'=>$blog,
+        'randomBlogs'=>Blog::inrandomOrder()->take(3)->get()
     ]);
 });
 Route::get('/categories/{category:slug}', function(Category $category){
     return view('blogs',[
-        'blogs'=>$category->blogs
+        'blogs'=>$category->blogs,
+        'categories'=>Category::all(),
+        'currentCategory'=>$category
     ]);
 });
 Route::get('/users/{user:username}', function(User $user){
     return view('blogs',[
-        'blogs'=>$user->blogs
+        'blogs'=>$user->blogs,
+        'categories'=>Category::all()
     ]);
 });
