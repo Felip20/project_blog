30d14d7d9ff80440887b50ec9c1c7fcee9df1638
wathone <mg.wathonekhant@gmail.com>
2023-11-27 19:47:02 +0630

    education

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,6 +1,6 @@
 <section class="px-4 py-5 my-5 text-center">
     <img class="d-block mx-auto mb-4" src="/images/logo.gif" alt="" width="250" />
-    <h1 class="display-5 fw-bold">Edcation Blog For Students</h1>
+    <h1 class="display-5 fw-bold">Education Blog For Students</h1>
     <div class="col-lg-6 mx-auto">
       <p class="lead mb-4 lh-lg mt-4">
         Learning and Exploring about the Blog</>
